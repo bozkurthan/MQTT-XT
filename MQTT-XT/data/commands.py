426ca99e49ffe33a2dfcae4ab0882d3fe508131e
bzokurthan <burakhancorak.ws@gmail.com>
2022-11-23 13:26:48 +0300

    Initial

---
+++ MQTT-XT/data/commands.py
@@ -0,0 +1,15 @@
+class Commands:
+    takeoff = dict(type="Ford", model="Mustang", year=1964)
+    go_to_position = dict(type="Ford", model="Mustang", year=1964)
+    land = dict(type="Ford", model="Mustang", year=1964)
+    return_to_home = dict(type="Ford", model="Mustang", year=1964)
+    start_mission = dict(type="Ford", model="Mustang", year=1964)
+    change_mode = dict(type="Ford", model="Mustang", year=1964)
+
+
+class Connect:
+    init = dict(connect="1", disconnect="0")
+
+    def connection_result(self,fog_name,result):
+        reachable_drones=result
+        return reachable_drones
